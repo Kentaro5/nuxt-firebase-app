edd838fddc0e75f78727c6a6463b4f37ef0a52f0
kentarominami <kentaro.minami@yourmystar.jp>
2022-05-05 15:57:10 +0900

    firebase回りのセットアップ

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,3 +1,20 @@
+<script setup lang="ts">
+import {getApps, initializeApp} from "firebase/app";
+
+if (getApps().length === 0) {
+  const runtimeConfig = useRuntimeConfig()
+// Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: runtimeConfig.FIREBASE_API_KEY,
+    authDomain: runtimeConfig.FIREBASE_AUTH_DOMAIN,
+    projectId: runtimeConfig.FIREBASE_PROJECT_ID,
+    storageBucket: runtimeConfig.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: runtimeConfig.FIREBASE_MESSAGING_SENDER_ID,
+    appId: runtimeConfig.FIREBASE_APP_ID,
+  };
+  initializeApp(firebaseConfig);
+}
+</script>
 <template>
   <div>
     <NuxtLayout>
