593aaa81c81b2d42d80e5f2dc2335dbaf8124ef6
kentarominami <kentaro.minami@yourmystar.jp>
2022-05-12 20:56:49 +0900

    ログアウトボタン変更

--- src/app/pages/index.vue
+++ src/app/pages/index.vue
@@ -1,16 +1,11 @@
 <script setup lang="ts">
-import { useSignOut } from '~/composables/auth'
-console.log('ログイン後のページ')
-const { signOut } = useSignOut()
 </script>
 <template>
   <div>
     <div class="flex place-content-center items-center">
       <div class="shadow-md max-360-none space-y-2 p-4">
         <p>ログイン後のページ</p>
-        <form class="" @submit.prevent="signOut">
-          <button class="bg-green-400 block text-white rounded-sm px-4 py-2" type="submit">ログアウト</button>
-        </form>
+        <nuxt-link to="/sign-out" class="bg-green-400 block text-white rounded-sm px-4 py-2">ログアウト</nuxt-link>
         <nuxt-link to="/mypage">マイページ</nuxt-link>
       </div>
     </div>
